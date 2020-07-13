440fe386ea8b748069568ca2f5cb319905413013
fritz duchardt <fritz@duchardt.net>
2020-07-13 19:16:45 +0200

    Added space filler (2nd)

--- src/main/java/com/fduchardt/k8sshowcase/web/K8sShowcaseController.java
+++ src/main/java/com/fduchardt/k8sshowcase/web/K8sShowcaseController.java
@@ -13,51 +13,52 @@ import java.util.stream.*;
 @Slf4j
 public class K8sShowcaseController {
 
-    @GetMapping(path="/env/{envVariable}")
+    @GetMapping(path = "/env/{envVariable}")
     public String getEnvVariable(@PathVariable String envVariable) {
         return System.getenv(envVariable);
     }
 
-    @GetMapping(path="/list/{folderPath}")
+    @GetMapping(path = "/list/{folderPath}")
     public String getFolder(@PathVariable String folderPath) throws IOException {
         log.info("list folder: {}", folderPath);
         Stream<Path> list = Files.list(Path.of(File.separatorChar + folderPath));
         return list.map(p -> p.toFile().getPath() + "\n").collect(Collectors.joining());
     }
 
-    @GetMapping(path="/content/{filePath}/{fileName}")
+    @GetMapping(path = "/content/{filePath}/{fileName}")
     public String getContent(@PathVariable String filePath, @PathVariable String fileName) throws IOException {
         String completeFilePath = File.separatorChar + filePath + File.separatorChar + fileName;
         log.info("content file: {}", completeFilePath);
         return Files.readString(Path.of(completeFilePath));
     }
 
-    @GetMapping(path="/hostname")
+    @GetMapping(path = "/hostname")
     public String getHostname() {
         log.info("Queried hostname");
         return System.getenv("HOSTNAME");
     }
 
-    @GetMapping(path="/cpu/{iterations}")
+    @GetMapping(path = "/cpu/{iterations}")
     public String cpu(@PathVariable int iterations) {
         log.info("Called compute with {} iterations", iterations);
-        IntStream.range(0, iterations * 1000).parallel().forEach((i) -> Math.tan(Math.atan(Math.tan(Math.atan(i))))); ;
+        IntStream.range(0, iterations * 1000).parallel().forEach((i) -> Math.tan(Math.atan(Math.tan(Math.atan(i)))));
+        ;
         return "done";
     }
 
-    @GetMapping(path="/stackoverflow")
+    @GetMapping(path = "/stackoverflow")
     public void stackOverFlow() {
         log.info("Causing Stackoverflow");
         stackOverFlow();
     }
 
-    @GetMapping(path="/memory/{megabytes}")
+    @GetMapping(path = "/memory/{megabytes}")
     public String memory(@PathVariable int megabytes) throws InterruptedException {
         log.info("Called memory with {} megabytes", megabytes);
         StringBuilder thousandRandomChars = new StringBuilder();
-        IntStream.range(0, 1000).parallel().forEach((i) -> thousandRandomChars.append((int)(Math.random() * 256)));
+        IntStream.range(0, 1000).parallel().forEach((i) -> thousandRandomChars.append((int) (Math.random() * 256)));
         StringBuilder massiveString = new StringBuilder();
-        for(int i = 0; i < megabytes; i++) {
+        for (int i = 0; i < megabytes; i++) {
             massiveString.append(thousandRandomChars.toString());
             Thread.sleep(10);
 
@@ -65,18 +66,17 @@ public class K8sShowcaseController {
         return "done";
     }
 
-    @GetMapping(path="/space/{megabytes}")
-    public String space(@PathVariable int megabytes) throws IOException {
+    @GetMapping(path = "/space/{megabytes}")
+    public String space(@PathVariable int megabytes) throws IOException, InterruptedException {
         log.info("Called space with {} megabytes", megabytes);
         StringBuilder thousandRandomChars = new StringBuilder();
-        IntStream.range(0, 1000).parallel().forEach((i) -> thousandRandomChars.append((int)(Math.random() * 256)));
-        StringBuilder massiveString = new StringBuilder();
-        FileOutputStream fileOutputStream = new FileOutputStream(new File("giga-file"));
-        for(int i = 0; i < megabytes; i++) {
-            massiveString.append(thousandRandomChars);
-            fileOutputStream.write(massiveString.toString().getBytes());
+        IntStream.range(0, 1000000).forEach((i) -> thousandRandomChars.append((int) (Math.random() * 256)));
+        for (int i = 0; i < megabytes; i++) {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("/tmp/space-" + i));
+            fileOutputStream.write(thousandRandomChars.toString().getBytes());
+            fileOutputStream.close();
+            Thread.sleep(100);
         }
-        fileOutputStream.close();
         return "done";
     }
 }
